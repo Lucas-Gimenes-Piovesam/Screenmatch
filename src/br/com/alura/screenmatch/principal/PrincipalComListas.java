2d95e50b8341a051dff3b62640bf618fafc1d857
lucapneu <lucaspiovesam07@gmail.com>
2026-04-29 14:09:02 -0300

    arrumando lista

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -40,7 +40,8 @@ public class PrincipalComListas {
         Collections.sort(buscaPorArtista);
         System.out.println("depois de ordenar");
         System.out.println(buscaPorArtista);
-//teste2
+
+        Collections.sort();
 
     }
 }
