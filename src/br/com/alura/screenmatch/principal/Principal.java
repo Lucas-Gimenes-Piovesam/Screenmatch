c40ad0bc393335877345ee788ba87b7fd55fa8b7
lucapneu <lucaspiovesam07@gmail.com>
2026-04-29 13:46:14 -0300

    teste 2

--- src/br/com/alura/screenmatch/principal/Principal.java
+++ src/br/com/alura/screenmatch/principal/Principal.java
@@ -63,7 +63,7 @@ public class Principal {
         System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
         System.out.println(listaDeFilmes);
         System.out.println("toString do filme " + listaDeFilmes.get(0).toString());
-
+//teste
 
 
 
